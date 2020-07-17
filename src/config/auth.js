a8c62af744709b7e6232956f546fc810b6765e24
wendelleloi <wendelleloi2@gmail.com>
2020-07-16 23:49:44 -0300

    :sparkles: Add user authetication

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'eb35390eaef1fbe9b405eafdc2317b75',
+  expireIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => {
   return res.json({ hello: 'word' });
 });
+
+routes.post('/sessions', SessionController.store);
 
 export default routes;
